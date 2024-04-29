a3d9a0ab93de62a6e1cfc835c62f8c9e2318007b
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-04-29 01:01:41 -0700

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,23 +3,19 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
-  [SerializeField]
-  private Vector2Int enemyNoRange = new Vector2Int(10, 20);
-  [SerializeField]
-  private GameObject enemyPrefab;
-  [SerializeField]
-  private Transform playerTransform;
+  [SerializeField] private Vector2Int enemyNoRange = new Vector2Int(10, 20);
+  [SerializeField] private GameObject enemyPrefab;
+  [SerializeField] private Transform playerTransform;
 
-  private List<EnemyScript> enemies = new List<EnemyScript>();
+  private List<EnemyScript> enemies = new();
 
   public void SpawnEnemies(int cell_size, int gridSize) {
     int n_enemies = Random.Range(enemyNoRange.x, enemyNoRange.y);
     for (int i = 0; i < n_enemies; i++) {
-      Vector3 pos = new Vector3(Random.Range(0, gridSize) * cell_size, 0f,
-                                Random.Range(2, gridSize) * cell_size);
-      EnemyScript enemy =
-          Instantiate(enemyPrefab, pos, Quaternion.identity, transform)
-              .GetComponent<EnemyScript>();
+      int x = Random.Range(0, gridSize) * cell_size;
+      int z = Random.Range(2, gridSize) * cell_size;
+      Vector3 pos = new(x, 0, z);;
+      EnemyScript enemy = Instantiate(enemyPrefab, pos, Quaternion.identity, transform).GetComponent<EnemyScript>();
       enemy.SetTarget(playerTransform);
       enemies.Add(enemy);
     }
