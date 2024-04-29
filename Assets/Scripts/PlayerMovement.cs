b1d065cf198dd4f2403ae1e27ffdb418c3ddf304
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-04-29 04:22:53 -0700

    Rigidbody based movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,48 +1,61 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-  [SerializeField] private CharacterController controller;
+
+  [Header("Movement Controls")]
+  [SerializeField] private Rigidbody rb;
   [SerializeField] private float speed = 12f;
-  [SerializeField] private float gravity = -9.81f;
+
+  [Header("Jumping Controls")]
+  [SerializeField] private float gravity = 9.81f;
   [SerializeField] private Transform groundCheck;
   [SerializeField] private float groundDistance = 2.4f;
-  [SerializeField] private float jumpHeight = 3f;
+  [SerializeField] private float jumpForce = 800;
+  [Range(0f, 1f)] [SerializeField] float stopFactor = 0.5f;
   [SerializeField] private LayerMask groundMask;
   [SerializeField] private AudioSource footSteps;
 
-  // Camera Movement
-  [SerializeField] private MouseMovment cameraMovement;
-
-  private Vector3 velocity = Vector3.zero;
+  // Private variables
+  private Vector3 velocity;
   private bool isGrounded = true;
-  private Vector3 movement = Vector3.zero;
+  private bool canJump = false;
+  private Vector3 movement;
 
   void GetInputs(){
+    float x = Input.GetAxisRaw("Horizontal");
+    float z = Input.GetAxisRaw("Vertical");
+    movement = new Vector3(x, 0, z).normalized;
+
     isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-    float x = Input.GetAxis("Horizontal");
-    float z = Input.GetAxis("Vertical");
-    movement = (transform.right * x + transform.forward * z).normalized;
   }
 
   // Update is called once per frame
   void Update() {
-
     GetInputs();
-
-    // Movement
-    controller.Move(movement * speed * Time.deltaTime);
+    ManageSounds();
 
     // Jumping
     // if (isGrounded && velocity.y < 0) velocity.y = -2f;
     if (isGrounded && Input.GetButtonDown("Jump")) {
-      velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+      canJump = true;
     }
+    if(Input.GetButtonUp("Jump")){
+      rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y * stopFactor, rb.velocity.z);
+    }
+  }
 
-    // Gravity
-    velocity.y += gravity * Time.deltaTime;
-    controller.Move(velocity * Time.deltaTime);
+  void FixedUpdate(){
+    var velocity = rb.velocity;
+    velocity = transform.TransformDirection(movement) * (speed * Time.deltaTime);
+    velocity.y = rb.velocity.y;
+    velocity.y -= gravity * Time.deltaTime;
+    rb.velocity = velocity;
 
-    ManageSounds();
+    // Jumping
+    if (canJump){
+      rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+      canJump = false;
+    }
   }
 
   void ManageSounds(){
@@ -50,13 +63,15 @@ public class PlayerMovement : MonoBehaviour {
     footSteps.volume = movement.magnitude > 0.1 ? 0.5f : 0f;
   }
 
+  #region Obsolete
   // Triggers Should not exist on player. Use scripts on these objects
-  // void OnTriggerEnter(Collider other) {
-  //   if (other.transform.CompareTag("Portal")) {
-  //     AudioManager.instance.Play("whoosh");
-  //   }
-  //   if (other.transform.CompareTag("Finish")) {
-  //     GameManager.instance.GameDone(true);
-  //   }
-  // }
+  void OnTriggerEnter(Collider other) {
+    if (other.transform.CompareTag("Portal")) {
+      AudioManager.instance.Play("whoosh");
+    }
+    if (other.transform.CompareTag("Finish")) {
+      GameManager.instance.GameDone(true);
+    }
+  }
+  #endregion
 }
