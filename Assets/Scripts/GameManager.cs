231257140d72593502992bb7a3d4af75f1128e98
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-04-29 00:55:37 -0700

    Refactored GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,48 +9,98 @@ public struct UIElements{
   public TMP_Text timeTakenText;
   public TMP_Text timeTakenTitle;
   public TMP_Text winLoseText;
+  public TMP_Text instructions;
 }
 
 public class GameManager : MonoBehaviour {
 
-  [SerializeField] private GameObject[] gems;
-
+  // Reference to player
   [SerializeField] private Transform player;
+
+  // Reference to Maze Generator
   [SerializeField] private MazeGenerator mazeGenerator;
 
   [Header("Canvas controls")]
   [SerializeField] private GameObject gameoverCanvas;
   [SerializeField] private GameObject hudCanvas;
 
-  [Header("Stats")]
+  // Stats
   public UIElements mainUI;
 
-  [Header("Earth Shield")]
+  // Earth Sheild UI
   public GameObject earthShield;
+  // Final Portal to enable when gamedone
   public GameObject finalPortal;
 
+  // Gems
+  [SerializeField] private GameObject[] gems;
   public ElementType currentElement = ElementType.NONE;
   public HashSet<ElementType> inventory = new HashSet<ElementType>();
+  private int gemsPlaced = 0;
 
-  void Start(){
+  // Portal
+  public int tunnel = 0;
+  public List<Transform> portals;
+
+  #region Singleton and Game Setup
+  public static GameManager instance;
+  void Awake() {
+    if (instance == null)
+      instance = this;
+    else {
+      Destroy(gameObject);
+      return;
+    }
+    DontDestroyOnLoad(gameObject);
+
+    GameSetup();
+  }
+
+  void CanvasSettings(bool gameStart){
+    // Enable these
+    hudCanvas.SetActive(gameStart);
+    // Disable these
+    gameoverCanvas.SetActive(!gameStart);
+    finalPortal.SetActive(!gameStart);
+  }
+
+  // Setup the game
+  void GameSetup(){
+    CanvasSettings(true);
+    SpawnGems();
+  }
+  #endregion
+
+  private void Start(){
     inventory.Add(ElementType.NONE);
     earthShield.SetActive(false);
   }
 
-  public TMP_Text instructions;
+  #region Gems
+  private void SpawnGems(){
+    for (int i = 0; i < gems.Length; i++) {
+      // Spawn at pos
+      // Random x and z position
+      int x = Random.Range(5, mazeGenerator.gridDimensions.x) * mazeGenerator.scaling;
+      int z = Random.Range(5, mazeGenerator.gridDimensions.y) * mazeGenerator.scaling;
+      Vector3 pos = new Vector3(x, 2f, z);
+      gems[i] = Instantiate(gems[i], pos, Quaternion.Euler(-90f, 0f, 0f), transform);
+
+      // Set gems as triggers
+      var col = gems[i].GetComponent<Collider>();
+      col.isTrigger = true;
+    }
+  }
 
   public void AddElement(ElementType et) {
     inventory.Add(et);
     SetCurrentElement(et);
   }
 
-  public int tunnel = 0;
-  public List<Transform> portals;
-
   public void SetCurrentElement(ElementType et) {
     // Check to see if player has this element
     if (!inventory.Contains(et)) {
-      instructions.text = "You haven't found this element yet.";
+      mainUI.instructions.text = "You haven't found this element yet.";
       return;
     }
 
@@ -63,102 +113,52 @@ public class GameManager : MonoBehaviour {
       earthShield.SetActive(false);
     }
     
-    // Notify player
+    // TODO: Notify player (This is shit)
     player.GetComponent<Shooting>().SetElement(currentElement);
   }
-
-  void Update() {
-    TakeInput(false);
-  }
-
-  void TakeInput(bool debug=false){
-    if(!debug){
-      if (Input.GetKeyDown(KeyCode.Alpha1)) {
-        SetCurrentElement(ElementType.FIRE);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha2)) {
-        SetCurrentElement(ElementType.WATER);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha3)) {
-        SetCurrentElement(ElementType.AIR);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha4)) {
-        SetCurrentElement(ElementType.EARTH);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha5)) {
-        SetCurrentElement(ElementType.NONE);
-      }
-    }
-    // Debug
-    else{
-      if (Input.GetKeyDown(KeyCode.Alpha1)) {
-        AddElement(ElementType.FIRE);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha2)) {
-        AddElement(ElementType.WATER);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha3)) {
-        AddElement(ElementType.AIR);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha4)) {
-        AddElement(ElementType.EARTH);
-      }
-      if (Input.GetKeyDown(KeyCode.Alpha5)) {
-        AddElement(ElementType.NONE);
-      }
-    }
-  }
-
-  private int gemsPlaced = 0;
 
   public void GemPlaced() {
     gemsPlaced++;
     if (gemsPlaced >= 4) {
-      instructions.text =
+      mainUI.instructions.text =
           "You did it! Jump Into the Portal to go back to the future";
       finalPortal.SetActive(true);
     }
   }
+  #endregion
 
-  public static GameManager instance;
-
-  void Awake() {
-    if (instance == null)
-      instance = this;
-    else {
-      Destroy(gameObject);
-      return;
+  private void Update() {
+    if (Input.GetKeyDown(KeyCode.Alpha1)) {
+      SetCurrentElement(ElementType.FIRE);
     }
-
-    DontDestroyOnLoad(gameObject);
-
-    hudCanvas.SetActive(true);
-    gameoverCanvas.SetActive(false);
-    finalPortal.SetActive(false);
-
-    // Instantiate Gems
-    for (int i = 0; i < gems.Length; i++) {
-      int x = Random.Range(5, (int)(mazeGenerator.gridDimensions.x)) *
-              mazeGenerator.scaling;
-      int z = Random.Range(5, (int)(mazeGenerator.gridDimensions.y)) *
-              mazeGenerator.scaling;
-
-      Vector3 pos = new Vector3(x, 2f, z);
-      gems[i] =
-          Instantiate(gems[i], pos, Quaternion.Euler(-90f, 0f, 0f), transform);
-      var col = gems[i].GetComponent<Collider>();
-      // Set gems as triggers
-      col.isTrigger = true;
+    if (Input.GetKeyDown(KeyCode.Alpha2)) {
+      SetCurrentElement(ElementType.WATER);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha3)) {
+      SetCurrentElement(ElementType.AIR);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha4)) {
+      SetCurrentElement(ElementType.EARTH);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha5)) {
+      SetCurrentElement(ElementType.NONE);
     }
   }
 
+  private void LockCursor(bool toLock){
+    Cursor.visible = !toLock;
+    if(toLock) Cursor.lockState = CursorLockMode.Locked;
+    else Cursor.lockState = CursorLockMode.None;
+  }
+
+  // Call when game finished, with winning condition
+  // Very shitty code
   public void GameDone(bool won) {
     // Pause Time
     Time.timeScale = 0;
 
     // Cursor
-    Cursor.visible = true;
-    Cursor.lockState = CursorLockMode.None;
+    LockCursor(false);
 
     // Get Data for UI
     int health = player.GetComponent<Player>().health;
@@ -169,21 +169,18 @@ public class GameManager : MonoBehaviour {
     mainUI.enemiesKilledText.text = enemies_killed.ToString();
     mainUI.winLoseText.text = won ? "YOU WIN :) !!" : "You Lost :(";
 
-    // Canvas Updates
-    gameoverCanvas.SetActive(true);
-    hudCanvas.SetActive(false);
+    CanvasSettings(false);
   }
 
+  // Call When Restarting game
   public void GameRestart() {
-    // Canvas Updates
-    gameoverCanvas.SetActive(false);
-    hudCanvas.SetActive(true);
-
     // Time back to normal
     Time.timeScale = 1;
 
+    // Canvas Updates
+    CanvasSettings(true);
+
     // Cursor Setup again
-    Cursor.visible = false;
-    Cursor.lockState = CursorLockMode.Locked;
+    LockCursor(true);
   }
 }
